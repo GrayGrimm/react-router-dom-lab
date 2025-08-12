5c6401909e67add60868cda6bf877ab92d7d9dc0
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-12 15:37:06 -0400

    added nav bar links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
 import MailboxForm from "./components/MailboxForm/MailboxForm";
 import MailboxList from "./components/MailboxList/MailboxList";
+import NavBar from "./components/NavBar/NavBar";
 
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1>Welcome to the Post Office!</h1>
+    </>
+  );
 };
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,20 @@
+import { Link } from 'react-router';
 
 const NavBar = () => {
-    return(
+    return (
         <nav>
-            
-        </nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/mailboxes'>Mailboxes</Link>
+                </li>
+                <li>
+                    <Link to='/new-mailbox'>New Mailbox</Link>
+                </li>
+            </ul>
+        </nav >
     );
 };
-
 export default NavBar;
